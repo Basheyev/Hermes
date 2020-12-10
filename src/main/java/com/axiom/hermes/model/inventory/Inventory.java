5d88f0a31155b68cbbb1358a29d33ace6bc395ab
Bolat Basheyev <b.basheyev@gmail.com>
2020-12-10 20:14:55 +0600

    Basic inventory transaction added

--- src/main/java/com/axiom/hermes/model/inventory/Inventory.java
+++ src/main/java/com/axiom/hermes/model/inventory/Inventory.java
@@ -1,4 +1,109 @@
 package com.axiom.hermes.model.inventory;
 
+
+import com.axiom.hermes.model.inventory.entities.StockTransaction;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
 public class Inventory {
+
+    @Inject
+    EntityManager entityManager;
+
+    /**
+     * Регистрация покупки товара (приход)
+     * @param productID код товарной позиции
+     * @param amount количество
+     * @param price цена
+     * @return true - если проведена, false - если нет
+     */
+    @Transactional
+    public StockTransaction purchase(int productID, int amount, double price) {
+        StockTransaction purchaseTransaction = new StockTransaction(productID,
+                StockTransaction.SIDE_DEBIT,
+                StockTransaction.DEBIT_PURCHASE,
+                amount, price);
+        entityManager.persist(purchaseTransaction);
+        return purchaseTransaction;
+    }
+
+
+    /**
+     * Регистрация возврата товара (приход)
+     * @param productID код товарной позиции
+     * @param amount количество
+     * @param price цена
+     * @return true - если проведена, false - если нет
+     */
+    @Transactional
+    public StockTransaction saleReturn(int productID, int amount, double price) {
+        StockTransaction saleReturnTransaction = new StockTransaction(productID,
+                StockTransaction.SIDE_DEBIT,
+                StockTransaction.DEBIT_SALE_RETURN,
+                amount, price);
+        entityManager.persist(saleReturnTransaction);
+        return saleReturnTransaction;
+    }
+
+    /**
+     * Продажа товара (расход)
+     * @param productID код товарной позиции
+     * @param amount количество
+     * @param price цена
+     * @return true - если проведена, false - если нет
+     */
+    @Transactional
+    public StockTransaction sale(int productID, int amount, double price) {
+        StockTransaction saleTransaction = new StockTransaction(productID,
+                StockTransaction.SIDE_CREDIT,
+                StockTransaction.CREDIT_SALE,
+                amount, price);
+        entityManager.persist(saleTransaction);
+        return saleTransaction;
+    }
+
+    /**
+     * Возврат поставщику закупленного товара (расход)
+     * @param productID код товарной позиции
+     * @param amount количество
+     * @param price цена
+     * @return true - если проведена, false - если нет
+     */
+    @Transactional
+    public StockTransaction purchaseReturn(int productID, int amount, double price) {
+        StockTransaction purchaseReturnTransaction = new StockTransaction(productID,
+                StockTransaction.SIDE_CREDIT,
+                StockTransaction.CREDIT_PURCHASE_RETURN,
+                amount, price);
+        entityManager.persist(purchaseReturnTransaction);
+        return purchaseReturnTransaction;
+    }
+
+
+    /**
+     * Списание товара (расход)
+     * @param productID код товарной позиции
+     * @param amount количество
+     * @param price цена
+     * @return true - если проведена, false - если нет
+     */
+    @Transactional
+    public StockTransaction writeOff(int productID, int amount, double price) {
+        StockTransaction writeOffTransaction = new StockTransaction(productID,
+                StockTransaction.SIDE_CREDIT,
+                StockTransaction.CREDIT_WRITE_OFF,
+                amount, price);
+
+        entityManager.persist(writeOffTransaction);
+
+        return writeOffTransaction;
+    }
+
+    // Пересорт (regrade)
+    // public void regrade
+
 }
--- src/main/java/com/axiom/hermes/model/inventory/entities/StockTransaction.java
+++ src/main/java/com/axiom/hermes/model/inventory/entities/StockTransaction.java
@@ -11,20 +11,23 @@ import javax.persistence.Id;
 public class StockTransaction {
 
     //-------------------------------------------------------------------------------------
-    public static final int SIDE_DEBIT = 1;             // Приход (дебет)
-    public static final int SIDE_CREDIT = 2;            // Расход (кредит)
-    public static final int OP_PURCHASE = 1;            // Покупка у поставщика (приход)
-    public static final int OP_SALE = 2;                // Продажа клиенту (расход)
-    public static final int OP_SALE_RETURN = 3;         // Возврат от клиента (приход)
-    public static final int OP_PURCHASE_RETURN = 4;     // Возврат поставщику (расход)
-    public static final int OP_REGRADE = 5;             // Пересорт товара (приход/расход)
-    public static final int OP_WRITE_OF = 6;            // Списание товара (расход)
+    public static final int SIDE_DEBIT = 1;              // Приход (дебет)
+    public static final int SIDE_CREDIT = 2;             // Расход (кредит)
+
+    public static final int DEBIT_PURCHASE = 10;         // Покупка у поставщика (приход)
+    public static final int DEBIT_SALE_RETURN = 11;      // Возврат от клиента (приход)
+
+    public static final int CREDIT_SALE = 20;            // Продажа клиенту (расход)
+    public static final int CREDIT_PURCHASE_RETURN = 21; // Возврат поставщику (расход)
+    public static final int CREDIT_WRITE_OFF = 22;       // Списание товара (расход)
+
+    public static final int REGRADE = 30;                // Пересорт товара (приход/расход)
     //-------------------------------------------------------------------------------------
 
     @Id
     @GeneratedValue
     public long transactionID;       // код транзакции
-    public long timestamp;           // время транзакции в наносекундах
+    public long timestamp;           // время транзакции в миллисекундах
     public int productID;            // код товара
     public int side;                 // дебет/кредит
     public int operationCode;        // код операции
@@ -34,4 +37,96 @@ public class StockTransaction {
     public int userID;               // код пользователя внесшего запись
     public boolean deleted;          // транзакция помечена как удалененная
 
+
+    public StockTransaction() {}
+
+    public StockTransaction(int productID, int side, int opCode, int amount, double price) {
+        this.timestamp = System.currentTimeMillis();
+        this.productID = productID;
+        this.side = side;
+        this.operationCode = opCode;
+        this.amount = amount;
+        this.price = price;
+        this.deleted = false;
+    }
+
+    public long getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(long transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public int getOperationCode() {
+        return operationCode;
+    }
+
+    public void setOperationCode(int operationCode) {
+        this.operationCode = operationCode;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCounterpartyID() {
+        return counterpartyID;
+    }
+
+    public void setCounterpartyID(int counterpartyID) {
+        this.counterpartyID = counterpartyID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }
