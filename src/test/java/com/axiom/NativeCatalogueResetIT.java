515171d1f55c3a29b412f02ff2681150294d1388
Bolat Basheyev <b.basheyev@gmail.com>
2020-12-12 21:08:45 +0600

    Refactored

--- src/test/java/com/axiom/NativeCatalogueResetIT.java
+++
@@ -1,9 +0,0 @@
-package com.axiom;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCatalogueResetIT extends CatalogueResetTest {
-
-    // Execute the same tests but in native mode.
-}
